26e3f73f5542fab474ced3a0dbd2b1705a655838
Sera De La Rosa <sera.delarosa@outlook.com>
2025-03-18 12:14:57 -0400

    Completed hoot list functionality.

--- README.md
+++ README.md
@@ -17,3 +17,15 @@ VITE_BACK_END_SERVER_URL=http://localhost:3000
 ```
 
 Start the app with `npm run dev`.
+
+User stories
+Below are the user stories we will implement within Hoot:
+
+As a guest, I should be able to create an account.
+As a new user with an account, I should be able to log in to my account.
+As a user, I should be able to create a hoot post.
+As a user, I should be able to see a list of all hoots on a ‘List’ page.
+As a user, clicking on a hoot in the ‘List’ page should navigate me to a hoot’s ‘Details’ page to view information about a single hoot post and its associated comments.
+As a user, I should be able to add a comment on a hoot’s ‘Details’ page.
+As the author of a hoot, I should see a link to ‘Edit’ a hoot on the ‘Details’ page. Clicking on the link should direct me to an ‘Edit’ page where I can modify the hoot. Upon submitting the update, I should be redirected back to the ‘Details’ page.
+As the author of a hoot, I should see a button to ‘Delete’ a hoot on the ‘Details’ page. Clicking the button should delete the hoot and redirect me back to the ‘List’ page.
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router';
+
+import * as hootService from './services/hootService';
 
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
+import HootList from './components/HootList/HootList';
 
 import { UserContext } from './contexts/UserContext';
 
 const App = () => {
+  const [hoots, setHoots] = useState([]);
   const { user } = useContext(UserContext);
+
+  // lets you perform side effects in function components
+  // side effect: fetching data from api
+  // useEffect takes a function
+  // React will run this function after the component renders
+  useEffect( () => {
+    // service call 
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+
+      setHoots(hootsData);
+    };
+
+    if (user) fetchAllHoots();
+  }, [user] ); // dependency array
   
   return (
     <>
+     {/* navbar will show up at all points */}
       <NavBar/>
       <Routes>
         <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
+        { user ? (
+          <>
+          <Route path='/hoots' element={<HootList hoots={hoots}/>} /> 
+          </>
+        ) : (
+          <>
+          <Route path='/sign-up' element={<SignUpForm />} />
+          <Route path='/sign-in' element={<SignInForm />} />
+          </>
+        )
+        }
       </Routes>
     </>
   );
